652ca55a38db02a71f50486bb99221f0e660e47e
ahnafadib <ahnafadib6@gmail.com>
2021-12-17 21:51:59 +0600

    first commit

---
+++ database/firebase.js
@@ -0,0 +1,14 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA9lKLPcDs5yUzG3F7APRYRMxOcb31LZ2w",
+    authDomain: "reactnativefirebase-a4cd0.firebaseapp.com",
+    projectId: "reactnativefirebase-a4cd0",
+    storageBucket: "reactnativefirebase-a4cd0.appspot.com",
+    messagingSenderId: "983605451251",
+    appId: "1:983605451251:web:74aa26e3546b66cf239a86"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
